1a538e99e438e2a73198331764e49188959c58a0
PHILIP DANIEL <Philip.J.Daniel@ibm.com>
2021-10-13 13:28:20 -0400

    update gradle
     - update publishing mechanism
     - convert to androidX

--- app/src/main/java/com/adityaarora/liveedgedetection/app/MainActivity.java
+++ app/src/main/java/com/adityaarora/liveedgedetection/app/MainActivity.java
@@ -1,10 +1,10 @@
-package com.adityaarora.liveedgedetection;
+package com.adityaarora.liveedgedetection.app;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 
 import com.adityaarora.liveedgedetection.activity.ScanActivity;
